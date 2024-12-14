3b0f13649ab703867fffda75e2ea27d1d5545b1c
Mateusz Konopelski <ml.konopelski@gmail.com>
2024-12-14 19:53:14 +0100

    cleaner solution

--- 2024/5/1.py
+++ 2024/5/1.py
@@ -1,5 +1,7 @@
-rules_after = {}
-rules_prior = {}
+from collections import defaultdict
+
+rules_after = defaultdict(list)
+rules_prior = defaultdict(list)
 updates = []
 
 with open('2024/5/data') as f:
@@ -10,15 +12,8 @@ with open('2024/5/data') as f:
             continue
         if '|' in line:
             k, v = [int(r) for r in line.rstrip().split('|')]
-            if rules_after.get(k):
-                rules_after[k].append(v)
-            else:
-                rules_after[k] = [v]
-
-            if rules_prior.get(v):
-                rules_prior[v].append(k)
-            else:
-                rules_prior[v] = [k]
+            rules_after[k].append(v)
+            rules_prior[v].append(k)
         else:
             updates.append([int(r) for r in line.rstrip().split(',')])
 
@@ -55,3 +50,29 @@ for cu in correct_updates:
     total_sum += cu[len(cu) // 2]
 
 print(total_sum)
+
+
+## Quicker solution
+with open('2024/5/data') as f:
+    data = f.read().strip()
+
+rules_raw, updates_raw = data.split('\n\n')
+
+rules = defaultdict(set)
+for rule in rules_raw.split('\n'):
+    x, y = rule.split('|')
+    x, y = int(x), int(y)
+    rules[y].add(x)
+
+ans = 0
+for update in updates_raw.split('\n'):
+    update = [int(x) for x in update.split(',')]
+    ok = True
+    for i, x in enumerate(update):
+        for j, y in enumerate(update):
+            if i < j and y in rules[x]:
+                ok = False
+    if ok:
+        ans += update[len(update) // 2]
+
+print(ans)
